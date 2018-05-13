8da8081578530e415cba4dacd304861e21b114b4
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2018-05-13 17:11:19 +0200

    bigger multiplication

--- src/MathForEmilka/MathForEmilka/MultiplicationGenerator.cs
+++ src/MathForEmilka/MathForEmilka/MultiplicationGenerator.cs
@@ -4,8 +4,8 @@
     {
         public Equation Generate()
         {
-            int first = StaticRandom.Instance.Next(3) + 2;
-            int second = StaticRandom.Instance.Next(3) + 2;
+            int first = StaticRandom.Instance.Next(4) + 2;
+            int second = StaticRandom.Instance.Next(4) + 2;
 
             return new Equation()
             {
