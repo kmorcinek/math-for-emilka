3d7cf695e49a8875b294e4219adc0668f442f563
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2018-06-06 08:32:51 +0200

    Change generator

--- src/MathForEmilka/MathForEmilka/Runner.cs
+++ src/MathForEmilka/MathForEmilka/Runner.cs
@@ -20,14 +20,24 @@ namespace KMorcinek.MathForEmilka
 
                     string line = Console.ReadLine();
 
-                    if (int.TryParse(line, out int result) && result == equation.Result)
+                    IEquationGenerator newGenerator = TryParseGenerator(line);
+
+                    if (newGenerator != null)
                     {
-                        Console.WriteLine("Dobrze!");
+                        generator = newGenerator;
                         break;
                     }
                     else
                     {
-                        Console.WriteLine("Sprobuj jeszcze raz");
+                        if (int.TryParse(line, out int result) && result == equation.Result)
+                        {
+                            Console.WriteLine("Dobrze!");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sprobuj jeszcze raz");
+                        } 
                     }
                 }
             }
@@ -41,18 +51,29 @@ namespace KMorcinek.MathForEmilka
             {
                 string line = Console.ReadLine();
 
-                switch (line)
+                IEquationGenerator equationGenerator = TryParseGenerator(line);
+
+                if (equationGenerator != null)
                 {
-                    case "+":
-                        return new AdditionGenerator();
-                    case "-":
-                        return new SubtractionGenerator();
-                    case "*":
-                        return new MultiplicationGenerator();
-                    default:
-                        Console.WriteLine("Nierozpoznany znak, wybierz jeszcze raz (+ - *):");
-                        break;
+                    return equationGenerator;
                 }
+
+                Console.WriteLine("Nierozpoznany znak, wybierz jeszcze raz (+ - *):");
+            }
+        }
+
+        IEquationGenerator TryParseGenerator(string line)
+        {
+            switch (line)
+            {
+                case "+":
+                    return new AdditionGenerator();
+                case "-":
+                    return new SubtractionGenerator();
+                case "*":
+                    return new MultiplicationGenerator();
+                default:
+                    return null;
             }
         }
     }
